7716408c3ee136c852672c23f292c8507b13196f
waff13 <mojmail@inbox.lv>
2021-05-03 10:09:03 +0300

    Create Floor.java

---
+++ Sec06/CE033_CarpetCostCalculator/Floor.java
@@ -0,0 +1,16 @@
+package CE033_CarpetCostCalculator;
+
+public class Floor {
+
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        this.width = width >= 0 ? width : 0.0;
+        this.length = length >= 0 ? length : 0.0;
+    }
+
+    public double getArea() {
+        return (width * length);
+    }
+}
