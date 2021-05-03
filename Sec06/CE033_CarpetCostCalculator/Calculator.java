8ef6695af5d2800e7fbfbb021ae332a38eb34d32
waff13 <mojmail@inbox.lv>
2021-05-03 10:10:08 +0300

    Create Calculator.java

---
+++ Sec06/CE033_CarpetCostCalculator/Calculator.java
@@ -0,0 +1,16 @@
+package CE033_CarpetCostCalculator;
+
+public class Calculator {
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return (this.floor.getArea() * this.carpet.getCost());
+    }
+}
