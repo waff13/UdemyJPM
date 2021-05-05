7469ffe92e72a28e8acdbfdae66a5a6bd5da34cb
waff13 <mojmail@inbox.lv>
2021-05-05 09:17:39 +0300

    Create Cylinder.java

---
+++ Sec06/CE035_Cylinder/Cylinder.java
@@ -0,0 +1,19 @@
+package CE035_Cylinder;
+
+public class Cylinder extends Circle{
+
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * getHeight();
+    }
+}
