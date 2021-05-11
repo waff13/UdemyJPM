a1729efe03be23b04dc70c8982ce923108a26c13
waff13 <mojmail@inbox.lv>
2021-05-11 15:50:48 +0300

    Create Wall.java

---
+++ Sec07/CE037_Composition/Wall.java
@@ -0,0 +1,13 @@
+package CE037_Composition;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
