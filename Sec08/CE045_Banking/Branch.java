b3feeeeeb0d59a0c1835972f9531fc5c58149605
waff13 <mojmail@inbox.lv>
2021-07-13 14:50:52 +0300

    Create Branch.java

---
+++ Sec08/CE045_Banking/Branch.java
@@ -0,0 +1,52 @@
+package CE045_Banking;
+
+import java.util.ArrayList;
+
+public class Branch {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch (String name) {
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public Customer findCustomer (String name) {
+        // Returns Customer if it exists or null if it does not
+        for (int i = 0; i < customers.size(); i++) {
+            Customer beingChecked = this.customers.get(i);
+            if (beingChecked.getName().equals(name)) {
+                return beingChecked;
+            }
+        }
+        return null;
+    }
+
+    public boolean newCustomer (String name, double initialAmount) {
+        // Returns true if customer was added or false if it did not
+        Customer existingCustomer = findCustomer(name);
+        if (existingCustomer == null) {
+            customers.add(new Customer(name, initialAmount));
+        }
+        return false;
+    }
+    
+    public boolean addCustomerTransaction (String name, double amount) {
+        // Returns true if transaction was added or false if it did not
+        Customer existingCustomer = findCustomer(name);
+        if (existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+        return false;
+    }
+}
